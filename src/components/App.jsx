7c19976b6d296ff0f4290e3915ea96c02573b996
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-11 22:57:34 -0500

    used setFullName as a function to see previous Value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,24 @@ function App() {
     lName: ""
   });
 
-  function handleChange() {}
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -18,13 +35,13 @@ function App() {
           name="fName"
           onChange={handleChange}
           placeholder="First Name"
-          value={fullName.fName}
+          // value={fullName.fName}
         />
         <input
           name="lName"
           onChange={handleChange}
           placeholder="Last Name"
-          value={fullName.lName}
+          // value={fullName.lName}
         />
         <button>Submit</button>
       </form>
